cc7c3b90e8baaa0d06f13dcc5493958381337919
mgalinski10 <michalg359@gmail.com>
2024-12-30 13:25:20 +0100

    add fetching notes

--- frontend/src/pages/StickyWallPage/StickyWallPage.jsx
+++ frontend/src/pages/StickyWallPage/StickyWallPage.jsx
@@ -4,25 +4,12 @@ import StickyNote from "../../components/StickyNote/StickyNote";
 import AddStickyNote from "../../components/AddStickyNote/AddStickyNote";
 import StickyNoteForm from "../../components/StickyNoteForm/StickyNoteForm";
 import { useStickyWall } from "../../context/StickyWallContext";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
 
 const StickyNotesPage = () => {
-  const { isOpen, openForm, activeNote } = useStickyWall();
-  const [notes, setNotes] = useState([]);
+  const { isOpen, openForm, activeNote, fetchNotes, notes } = useStickyWall();
 
   useEffect(() => {
-    const fetchNotes = async () => {
-      try {
-        const response = await axios.get("http://localhost:5000/api/notes", {
-          withCredentials: true,
-        });
-        setNotes(response.data);
-      } catch (err) {
-        console.log(`Error while fetching tasks: ${err}`);
-      }
-    };
-
     fetchNotes();
   }, []);
 
