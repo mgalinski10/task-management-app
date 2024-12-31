3d1ddbfba6852020eb8a3eaf40d18e69c3086415
mgalinski10 <michalg359@gmail.com>
2024-12-31 13:57:34 +0100

    prepare UserForm to data fetching

--- frontend/src/components/UserForm/UserForm.jsx
+++ frontend/src/components/UserForm/UserForm.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./UserForm.module.scss";
+import Button from "../Button/Button";
 
 const UserForm = () => {
+  const [nickname, setNickname] = useState("");
+  const [gender, setGender] = useState("");
+  const [country, setCountry] = useState("");
+
   return (
     <form>
       <ul className={styles.labels}>
         <li className={styles.item}>
           <label>Nickname</label>
-          <input placeholder="Enter your Nickname" />
+          <input
+            placeholder="Enter your Nickname"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
         </li>
         <li className={styles.item}>
           <label>Gender</label>
-          <select>
+          <select value={gender} onChange={(e) => setGender(e.target.value)}>
             <option value="">Select...</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
@@ -19,11 +28,16 @@ const UserForm = () => {
         </li>
         <li className={styles.item}>
           <label>Country</label>
-          <select>
+          <select value={country} onChange={(e) => setCountry(e.target.value)}>
             <option value="">Select...</option>
-            <option value="gdynia">Poland</option>
-            <option value="gdansk">United States</option>
+            <option value="poland">Poland</option>
+            <option value="usa">United States</option>
           </select>
+        </li>
+        <li className={styles.button}>
+          <Button type="submit" backgroundColor="rgb(254, 215, 19)">
+            Save
+          </Button>
         </li>
       </ul>
     </form>
